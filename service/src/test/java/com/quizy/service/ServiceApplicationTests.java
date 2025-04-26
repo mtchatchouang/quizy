6a5dc1e8878e5bf520b25091a58066ba56d3dc91
Maturin <maturin.tchatchouang@gmail.com>
2025-04-26 16:35:17 +0200

    Setup Spring Boot Service directory

---
+++ service/src/test/java/com/quizy/service/ServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.quizy.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+    // This test is used to check if the Spring application context loads successfully.
+    // If there are any issues with the application context, this test will fail.
+    // No additional code is needed here.
+  }
+
+}
